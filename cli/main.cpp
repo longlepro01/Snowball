63157854fff576475c64e991541f6636f5903dd5
longlepro01 <le.long@brentwood.ca>
2025-10-03 20:56:22 +0700

    added json parsing function for goal

--- cli/main.cpp
+++ cli/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <termios.h>
+#include <unistd.h>
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
 
 
 struct Task {
@@ -21,7 +26,6 @@ struct Goal {
   std::string title;
   std::string description;
   std::vector<Segment> segments;
-  int status;
 };
 
 std::vector<Goal> goals;
@@ -39,7 +43,7 @@ void userCommandInput() {
 void renderDashboard() {
   std::cout << "\n❄️  Snowball – Task-centric Goal Tracker\n";
   std::cout << "--------------------------------------\n";
-  std::cout << "\nTODO: add overview of all goals.\n\n\n\n\n\n";
+  std::cout << "\nTODO: implement overview of all goals.\n\n\n\n\n\n";
   std::cout << "Add [G]oals \t";
   std::cout << "[M]anage goals \t";
   std::cout << "Random [T]ask \t";
@@ -49,9 +53,15 @@ void renderDashboard() {
 
 void renderAddGoals() {
   std::string title;
+  std::string description;
+  std::cout << "\n❄️  Snowball – Task-centric Goal Tracker\n";
+  std::cout << "--------------------------------------\n";
+  std::cout << "\nTODO: implement add goal view.\n\n\n\n\n\n";
   std::cout << "Enter goal title: ";
   std::getline(std::cin, title);
-  goals.push_back({title, {}});
+  std::cout << "Enter goal description (can be empty): "
+  std::getline(std::cin, description);
+  goals.push_back({title, description, {}});
   systemMsg = "✅ Goal added!";
 }
 
@@ -65,10 +75,38 @@ void renderRandomTask() {
   std::cout << "TODO: Develop random task functions\n";
 }
 
+// Parse Segment
+// void from_json(const json& j, Segment& s) {
+//     j.at("title").get_to(s.title);
+//     j.at("description").get_to(s.description);
+//     j.at("completed").get_to(s.completed);
+// }
+
+// Parse Goals
+void from_json(const json& j, Goal& g) {
+    j.at("title").get_to(g.title);
+    j.at("description").get_to(g.description);
+    j.at("segments").get_to(g.segments); // vector<Segment> handled automatically
+}
+std::vector<Goal> loadData() {
+  // TODO: Implement loading data from json file
+  try {
+    json j = json::parse()
+  }
+  std::vector<Goal> = 
+
+ 
+}
+
+void saveData() {
+  // TODO: Implement writing data to json file
+}
+
 int main() {
   char choice = ' ';
-
-  while (true) {
+  bool running = true;
+  
+   while (running) {
     renderDashboard();
     std::cin >> choice;
     std::cin.ignore(); // clear input buffer
@@ -77,8 +115,10 @@ int main() {
       case 'g': renderAddGoals(); break;
       case 'm': renderManageGoal(); break;
       case 't': renderRandomTask(); break;
-      case 'q': std::cout << "Goodbye!\n"; return 0;
+      case 'q': saveData(); std::cout << "Goodbye!\n"; running = false;
       default: systemMsg = "Invalid input!";
     }
   }
+
+  // TODO: save data to json file (../backend/data)
 }
