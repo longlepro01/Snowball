46c97e4ad31efa0f301fcb457ab328f6495893f1
longlepro01 <le.long@brentwood.ca>
2025-10-03 18:00:03 +0700

    initialized cli interface, started on dashboard and add goal, set up buildmake files for easy compile + run

---
+++ cli/build/CMakeFiles/snowball.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/snowball.dir/main.cpp.o"
+  "CMakeFiles/snowball.dir/main.cpp.o.d"
+  "snowball"
+  "snowball.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/snowball.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cli/main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+struct Task {
+  std::string title;
+  std::string description;
+  Task* nextTask = nullptr;
+  bool done = false;
+};
+
+struct Segment {
+  std::string title;
+  std::string description;
+  std::vector<Task> tasks;
+  int status;
+};
+
+struct Goal {
+  std::string title;
+  std::string description;
+  std::vector<Segment> segments;
+  int status;
+};
+
+std::vector<Goal> goals;
+std::string systemMsg;
+
+void userCommandInput() {
+  if (!systemMsg.empty()) {
+    std::cout << "System Message: " + systemMsg << std::endl;
+  } else {
+    std::cout << std::endl;
+  }
+  std::cout << "Choose an option: ";
+}
+
+void renderDashboard() {
+  std::cout << "\n❄️  Snowball – Task-centric Goal Tracker\n";
+  std::cout << "--------------------------------------\n";
+  std::cout << "\nTODO: add overview of all goals.\n\n\n\n\n\n";
+  std::cout << "Add [G]oals \t";
+  std::cout << "[M]anage goals \t";
+  std::cout << "Random [T]ask \t";
+  std::cout << "[Q]Exit\n";
+  userCommandInput();
+}
+
+void renderAddGoals() {
+  std::string title;
+  std::cout << "Enter goal title: ";
+  std::getline(std::cin, title);
+  goals.push_back({title, {}});
+  systemMsg = "✅ Goal added!";
+}
+
+int main() {
+  char choice = ' ';
+
+  while (true) {
+    renderDashboard();
+    std::cin >> choice;
+    std::cin.ignore(); // clear input buffer
+
+    switch (choice) {
+      case 'g': renderAddGoals(); break;
+      case 'q': std::cout << "Goodbye!\n"; return 0;
+      default: systemMsg = "Invalid input!";
+    }
+  }
+}
